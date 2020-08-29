ab701284d3a6fbd6567897b38ea765f5922553e0
ABYA <abhishek.vn303@gmail.com>
2020-08-29 18:08:21 -0400

    Client service registered with Eureka Service registry

---
+++ course2/dogmicroservice/src/main/java/com/udacity/dogmicroservice/DogmicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.dogmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DogmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogmicroserviceApplication.class, args);
+	}
+}
